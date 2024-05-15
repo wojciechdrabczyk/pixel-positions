af24eaf9c4692b726e58303a133903d44eb15357
wojciechdrabczyk <wojtek@lycal.dev>
2024-05-15 12:17:12 +0200

    feat: implement search and search tags

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * @method static firstOrCreate(string[] $array)
@@ -11,4 +12,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    public function jobs(): BelongsToMany
+    {
+        return $this->belongsToMany(Job::class);
+    }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -28,9 +28,20 @@
             <a href="#">Salaries</a>
             <a href="#">Companies</a>
         </div>
-        <div>
-            <a href="">Post a Job</a>
-        </div>
+        @auth
+            <div>
+                <a href="/jobs/create">Post a Job</a>
+            </div>
+        @endauth
+
+        @guest
+            <div>
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Log In</a>
+                </div>
+            </div>
+        @endguest
     </nav>
 
     <main class="my-10 max-w-[986px] mx-auto">
